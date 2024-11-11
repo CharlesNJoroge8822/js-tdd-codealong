fe832e104a68afb2bc8ecb338fc0c867b3819ea3
njorogecharles439@gmail.com <njorogecharles439@gmail.com.>
2024-11-11 10:00:22 +0300

    changes

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -4,6 +4,9 @@ describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
     const birthYear = 1984;
     const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+
+    const expectedAge = new Date().getFullYear() - birthYear;
+    expect(ageOfPerson).toBe(expectedAge);
   });
 });
+
